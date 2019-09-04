7a8866f2001c3128ab413258a1f533e1d98a0f2c
Lucas Laroz <lucaslaroz@hotmail.com>
2019-09-03 21:23:19 -0300

    Add Support For Tag To Be Saved In MongoDB Schema

--- plugins/notifications/models/notifications.model.js
+++ plugins/notifications/models/notifications.model.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const NotificationsSchema = new Schema({
-  title: { type: String, required: true},
-  description: { type: String, required: true},
-  important: { type: Boolean, required: false},
-  schedule: { type: Date, required: false }
+  title: {type: String, required: true},
+  description: {type: String, required: true},
+  important: {type: Boolean, required: false},
+  schedule: {type: Date, required: false},
+  tag: {type: String, required: false}
 },
 {
   collection: 'notifications'
